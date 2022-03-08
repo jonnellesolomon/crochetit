568f9c8fd7a31c109abb39f5a14f21e6de23f289
Jonelle Solomon <53133444+jonnellesolomon@users.noreply.github.com>
2022-03-08 10:56:23 +0800

    1st

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function openForm2() {
+  document.getElementById("myForm2").style.display = "block";
+}
+function closeForm2() {
+  document.getElementById("myForm2").style.display = "none";
+}
+
+function openSearch() {
+  document.getElementById("mySearch").style.display = "block";
+  document.getElementById("magnif").style.display = "none";
+}
+function closeSearch() {
+  document.getElementById("mySearch").style.display = "none";
+  document.getElementById("magnif").style.display = "block";
+}
+  
